0c222b22e8d6d0dc04a4ade623973327dbcecade
etryfly <etrymax@gmail.com>
2020-11-06 23:37:57 +0300

    Done

--- src/Controller.java
+++ src/Controller.java
@@ -2,11 +2,15 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import javafx.application.Platform;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,9 +18,11 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.HTMLEditor;
 import javafx.scene.web.WebEngine;
-import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -293,5 +299,119 @@ public class Controller {
             e.printStackTrace();
         }
 
+    }
+
+    @FXML
+    public void refreshOnClick(ActionEvent actionEvent) {
+        Tab tab = tabPane.getSelectionModel().getSelectedItem();
+        if (tab != null) {
+            getWebViewInTab(tab).getEngine().reload();
+        }
+    }
+
+    @FXML
+    public void FavoritesOnClick(ActionEvent actionEvent) {
+        TableView<String> view = new TableView<>();
+        TableColumn<String, String> col = new TableColumn<>("URL");
+        col.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
+        ObservableList<String> data =
+                FXCollections.observableArrayList(favoriteUrls);
+
+        view.setItems(data);
+        AnchorPane pane = new AnchorPane();
+        view.getColumns().add(col);
+        pane.getChildren().add(view);
+        Button okButton = new Button("OK");
+        Button cancelButton = new Button("Cancel");
+        AnchorPane.setBottomAnchor(okButton, 30.);
+        AnchorPane.setBottomAnchor(cancelButton, 0.);
+        pane.getChildren().add(okButton);
+        pane.getChildren().add(cancelButton);
+
+
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(pane));
+        Tab tab = tabPane.getSelectionModel().getSelectedItem();
+        okButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                getWebViewInTab(tab).getEngine().load(view.getSelectionModel().getSelectedItem());
+                stage.close();
+            }
+        });
+
+        cancelButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                stage.close();
+            }
+        });
+        stage.showAndWait();
+
+
+
+
+
+    }
+
+    ArrayList<String> favoriteUrls = new ArrayList<>();
+
+
+
+    @FXML
+    public void AddToFavoriteOnClick(ActionEvent actionEvent) {
+        Tab tab = tabPane.getSelectionModel().getSelectedItem();
+        if (tab != null) {
+            favoriteUrls.add(getWebViewInTab(tab).getEngine().getLocation());
+        }
+    }
+
+
+    @FXML
+    public void htmlEditorOnClick(ActionEvent actionEvent) {
+        Tab tab = tabPane.getSelectionModel().getSelectedItem();
+        if (tab == null) return;
+        Stage stage = new Stage();
+        VBox pane = new VBox();
+        TextArea text = new TextArea();
+        MenuBar menuBar = new MenuBar();
+        Menu menu = new Menu("Save");
+        MenuItem item = new MenuItem("Save");
+        item.setOnAction(event -> {
+            FileChooser fileChooser = new FileChooser();
+
+            //Set extension filter for text files
+            FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("HTML files (*.html)", "*.html");
+            fileChooser.getExtensionFilters().add(extFilter);
+            Stage saveDialogStage = new Stage();
+            //Show save file dialog
+            File file = fileChooser.showSaveDialog(saveDialogStage);
+
+            if (file != null) {
+                try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))){
+
+                    writer.write(text.getText());
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            saveDialogStage.close();
+        });
+        menu.getItems().add(item);
+        menuBar.getMenus().add(menu);
+
+        pane.getChildren().addAll(menuBar, text);
+
+        stage.setScene(new Scene(pane));
+
+
+        WebView view = getWebViewInTab(tab);
+        text.setText( (String) view.getEngine().executeScript("document.documentElement.outerHTML"));
+
+
+        stage.showAndWait();
     }
 }
